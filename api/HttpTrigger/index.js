b65d140475cafbcc11591851d31954426297e1ac
Kazuki Ota <kaota@microsoft.com>
2021-04-18 11:01:48 +0900

    xxxx

--- api/HttpTrigger/index.js
+++ api/HttpTrigger/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, req) {
     const name = user.userDetails;
     // const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? process.env.GREETING_MESSAGE + ", " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
